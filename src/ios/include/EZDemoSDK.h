617679e542c8a8cdb3ccdba15b65dd3647fc6fbe
21334878@qq.com <21334878@qq.com>
2018-06-13 18:08:54 +0800

    add iOS code

---
+++ src/ios/include/EZDemoSDK.h
@@ -0,0 +1,24 @@
+//
+//  EZDemoSDK.h
+//  EZDemoSDK
+//
+//  Created by MrMessy on 2017/6/16.
+//  Copyright © 2017年 MrMessy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EZDemoSDK.
+FOUNDATION_EXPORT double EZDemoSDKVersionNumber;
+
+//! Project version string for EZDemoSDK.
+FOUNDATION_EXPORT const unsigned char EZDemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZDemoSDK/PublicHeader.h>
+#import "EZOpenSDK.h"
+#import "EZDeviceInfo.h"
+#import "EZCameraInfo.h"
+#import "EZLivePlayViewController.h"
+#import "EZCameraTableViewController.h"
+#import "EZAddByQRCodeViewController.h"
+#import "EZWifiConfigViewController.h"
